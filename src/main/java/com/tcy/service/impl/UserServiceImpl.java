86c17d914325af8c3ac004b92c5860b9babb32b9
tongchengyu <1429920627@qq.com>
2021-03-06 20:45:50 +0800

    添加事务

--- src/main/java/com/tcy/service/impl/UserServiceImpl.java
+++ src/main/java/com/tcy/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.tcy.domain.User;
 import com.tcy.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,16 +26,19 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public int saveUser(User user) {
         return userDao.saveUser(user);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public int updateUser(User user) {
         return userDao.updateUser(user);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public int removeUser(Long id) {
         return userDao.removeUser(id);
     }
